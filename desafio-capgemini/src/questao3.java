f8709b3b57ca16ed06a57e0fb525d6119325edb3
Jackson Soares <jackson33.soares@gmail.com>
2022-02-19 20:47:30 -0300

    Update questao3.java

--- desafio-capgemini/src/questao3.java
+++ desafio-capgemini/src/questao3.java
@@ -1,4 +1,4 @@
-//CÛdigo referente ‡ segunda quest„o do desafio de programaÁ„o - Academia Capgemini!
+//C√≥digo referente √† terceira quest√£o do desafio de programa√ß√£o - Academia Capgemini!
 
 import java.util.Arrays;
 
@@ -9,31 +9,31 @@ public class questao3 {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
-		/*DeclaraÁ„o da variavel tipo Scanner que grava a entrada de dados do usu·rio*/
+		/*Declara√ß√£o da variavel tipo Scanner que grava a entrada de dados do usu√°rio*/
 		Scanner leia = new Scanner(System.in);
 	
 		System.out.println("Digite a palavra: ");
 		
-		/*DeclaraÁ„o da vari·vel que ir· receber a palavra digitada pelo o usu·rio
-		conforme pedido no enunciado da quest„o, tambÈm h· a declaraÁ„o de uma outra
-		variavel do tipo String no e que atuar· como auxiliar para verificar se a 
+		/*Declara√ß√£o da vari√°vel que ir√° receber a palavra digitada pelo o usu√°rio
+		conforme pedido no enunciado da quest√£o, tamb√©m h√° a declara√ß√£o de uma outra
+		variavel do tipo String no e que atuar√° como auxiliar para verificar se a 
 		palavra possui anagramas*/
 		
 		String palavra = leia.nextLine();
 		
 		String aux = palavra;
 		
-		/*DeclaraÁ„o da vari·vel contador que retornar· a quantidade de anagramas na 
+		/*Declara√ß√£o da vari√°vel contador que retornar√° a quantidade de anagramas na 
 		 palavra*/
 		
 		int contador=0;
 		
 		
-		/*Na lÛgica implementada foram feitos dois For, o For mais externo (o primeiro
-		no caso), ir· percorrer a string do primeiro caractere atÈ a metade da palavra
+		/*Na l√≥gica implementada foram feitos dois For, o For mais externo (o primeiro
+		no caso), ir√° percorrer a string do primeiro caractere at√© a metade da palavra
 		inicia fazendo o teste individual com a primeira letra e as demais para verificar
-		a ocorrencia, da mesma letra o teste È realizado no For mais interno e assim 
-		apÛs percorrer toda a palavra testando as ocorrencias o algoritmo passa ‡ 
+		a ocorrencia, da mesma letra o teste √© realizado no For mais interno e assim 
+		ap√≥s percorrer toda a palavra testando as ocorrencias o algoritmo passa √† 
 		segunda letra e passa a testar de 2 em 2 depois de 3 em 3 e assim sucessivamente
 		ao final retorna a quantidade de anagramas encontrados e exibe o resultado*/
 		
@@ -48,15 +48,15 @@ public class questao3 {
 			Arrays.sort(x);
 			Arrays.sort(y);
 			
-			/*ApÛs a ordenaÁ„o dos 2 vetores contendo partes da palavra digitada
-			ent„o È feito o teste de ocorrencia dos anagramas*/
+			/*Ap√≥s a ordena√ß√£o dos 2 vetores contendo partes da palavra digitada
+			ent√£o √© feito o teste de ocorrencia dos anagramas*/
 				
 			for (char c : x) {
 				
 				for(char d : y){
 						
 					/*Caso os caracteres armazenados,sejam exatamente iguais independente da 
-					ordem de ocorrencia ent„o o contador È incrementado*/
+					ordem de ocorrencia ent√£o o contador √© incrementado*/
 					
 					if((Character.compare(c, d)==0)){
 						contador=contador+1;
@@ -67,7 +67,7 @@ public class questao3 {
 			
 		}
 		
-		//Ao fim È exibido o resultado final de quantos anagramas a palavra possui
+		//Ao fim √© exibido o resultado final de quantos anagramas a palavra possui
 		
 		System.out.println(contador);
 	}		
